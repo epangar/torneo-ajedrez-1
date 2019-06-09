18744cc8ec6901677c8dcb2049d1e8640095819d
Alvaro Corral Soilan <alvaro.corral@innocv.com>
2019-06-09 21:44:52 +0200

    First components, parent, instructions, personal-data, and presentation

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PersonalDataComponent } from './personal-data/personal-data.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { InstructionsComponent } from './instructions/instructions.component';
+import { ParentComponent } from './parent/parent.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PersonalDataComponent,
+    PresentationComponent,
+    InstructionsComponent,
+    ParentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
